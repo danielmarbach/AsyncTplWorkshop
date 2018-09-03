25ef791d1a23f498aad67bde24391096a0def1eb
danielmarbach <daniel.marbach@openplace.net>
2018-09-03 09:27:35 +0200

    TaskFactoryStartNew and minor spelling

--- AsyncTplWorkshop/TaskFactoryStartNew.cs
+++ AsyncTplWorkshop/TaskFactoryStartNew.cs
@@ -4,21 +4,25 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 
 [TestFixture]
-public class TaskRun
+public class TaskFactoryStartNew
 {
     /*
      * Exercise:
-     *  Use `Task.Run` in combination with `CpuBound.Compute` to sort one array of size 10..13 in parallel
-     *  Discuss with your peers what happens and why
+     *  Use `Task.Factory.StartNew` in combination with `CpuBound.Compute` to sort one array of size 10..15 in parallel
+     *  Make Task.Factory.StartNew behave like Task.Run
+     *  Discuss with your peers what the difference is between Task.Run and Task.Factory.StartNew
      *
      * Bonus:
      *  - What happens to the thread of execution, is it blocked or not?
      *  - What could you change to address the above effect?
      *  - How could you achieve something similar to what we did with Parallel.For?
      *  - How would you implement the parallelism limitation?
+     *  - Create a new TestFixture with parent and child tasks, use `DenyChildAttach` and `Task.Run` 
+     *    as well as `Task.Factory.StartNew` to observe how it influences the runtime behavior
      *
      * Hint:
      *  - You can use `PrintCurrentThread` or other Helpers to print i.ex. the current managed thread ID
+     *  - You might need to use dotPeek, .NET Reflector or references sources
      */
     private Task[] ImplementHere()
     {
