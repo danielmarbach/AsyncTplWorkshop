4f65dfa3178d285bb3dd9364fe6c3b6b4cf8de10
danielmarbach <daniel.marbach@openplace.net>
2018-09-04 20:28:54 +0200

    Even bigger exercise

---
+++ AsyncTplWorkshop/MultiProducerConcurrentConsumer/MultiProducerConcurrentCompletion.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncTplWorkshop.MultiProducerConcurrentConsumer
+{
+    /// <summary>
+    /// This is essentially a reactive collection which allows to push elements to multiple slots. When either the batchSize is
+    /// reached or the pushInterval
+    /// then the data if available is pushed to the callback which is passed into the start method. The callback is invoked per
+    /// slot up to the specified maximum concurrency per slot.
+    /// The implementation makes a number of trade-offs by preallocating the item lists for the specified maximum concurrency
+    /// and available slots.
+    /// </summary>
+    /// <typeparam name="TItem">The item that is managed by the dispatcher.</typeparam>
+    class MultiProducerConcurrentCompletion<TItem>
+    {
+        private int maxConcurrency;
+        private TimeSpan pushInterval;
+        private int batchSize;
+        private int numberOfSlots;
+
+        public MultiProducerConcurrentCompletion(int batchSize, TimeSpan pushInterval, int maxConcurrency,
+            int numberOfSlots)
+        {
+            this.maxConcurrency = maxConcurrency;
+            this.pushInterval = pushInterval;
+            this.batchSize = batchSize;
+            this.numberOfSlots = numberOfSlots;
+        }
+
+        /// <summary>
+        /// Specifies a pump function. As soon as items are available pumping begins within the specified constraints
+        /// </summary>
+        /// <remarks>This member is not thread safe.</remarks>
+        public void Start(Func<List<TItem>, int, object, CancellationToken, Task> pump)
+        {
+            Start(pump, null);
+        }
+
+        /// <summary>
+        /// Specifies a pump function. As soon as items are available pumping begins within the specified constraints
+        /// </summary>
+        /// <remarks>This member is not thread safe.</remarks>
+        public void Start(Func<List<TItem>, int, object, CancellationToken, Task> pump, object state)
+        {
+
+        }
+
+        /// <summary>
+        /// Pushes an item for the specified slot number. Pushing is allowed even when the dispatcher is not started.
+        /// </summary>
+        /// <param name="item">The item to be pushed.</param>
+        /// <param name="slotNumber">The slot number which is zero based.</param>
+        /// <remarks>This member is thread safe.</remarks>
+        public void Push(TItem item, int slotNumber)
+        {
+        }
+
+        /// <summary>
+        /// Completes the dispatching asynchronously. If necessary remaining items will be pushed asynchronously on the thread
+        /// entering this method.
+        /// It is possible to start the dispatching again but the pump function as well as the required state has to be passed in
+        /// again.
+        /// </summary>
+        /// <param name="drain">
+        /// Indicates whether remaing items in the slots should be drained and pushed to the listener.
+        /// Specifying <c>false</c> will empty the slots without pushing.
+        /// </param>
+        /// <remarks>This member is not thread safe.</remarks>
+        public Task Complete(bool drain = true)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ AsyncTplWorkshop/MultiProducerConcurrentConsumer/Spec.cs
@@ -0,0 +1,292 @@
+﻿namespace AsyncTplWorkshop.MultiProducerConcurrentConsumer
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    /*
+     * Bonus Exercise:
+        The multi producer concurrent consumer is a reactive collection with slots, batchSize, pushInterval and maximum concurrency. The guarantees are the following:
+       - It is fully asynchronous
+       - Pushing to it is thread safe
+       - It respects the batchSize and automatically fans out if there is more to complete for a given slot when there is still concurrency left
+       - It completes messages when they arrive slower under the predefined push interval
+       - Only ever one thread is executing the pump logic but the completions are faned out
+       - The pump callback will only receive data for a given slot
+       - No contention between slots
+       - Lock-free
+       - Allocates the necessary collections up front for the maxed out scenario. The only allocations you pay during runtime is a Tuple and the async statemachine
+       - Has FIFO semantics instead of LIFO
+
+        Implement the above requirements, below is a bunch of tests that help you drive the implementation
+
+        See also Overview.png
+     */
+    [TestFixture]
+    public class When_using_multiproducerconcurrentcompletion
+    {
+        [Test]
+        public async Task Pushing_for_slots_before_start_works_when_batch_size_is_reached()
+        {
+            var receivedItems = new ConcurrentQueue<List<int>>[4]
+            {
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+            };
+
+            var countDownEvent = new CountdownEvent(16);
+
+            // choose insanely high push interval
+            var completion = new MultiProducerConcurrentCompletion<int>(batchSize: 100, pushInterval: TimeSpan.FromDays(1), maxConcurrency: 4, numberOfSlots: 4);
+
+            var numberOfItems = await PushConcurrentlyTwoThousandItemsInPackagesOfFiveHundredIntoFourSlots(completion);
+
+            completion.Start((items, slot, state, token) =>
+            {
+                receivedItems[slot].Enqueue(new List<int>(items)); // take a copy
+                if (!countDownEvent.IsSet)
+                {
+                    countDownEvent.Signal();
+                }
+                return Task.FromResult(0);
+            });
+
+            await Task.Run(() => countDownEvent.Wait(TimeSpan.FromSeconds(5)));
+
+            var snapShotOfElementsBeforeComplete = Flatten(receivedItems).Count();
+
+            await completion.Complete();
+
+            Assert.AreEqual(1600, snapShotOfElementsBeforeComplete);
+            Assert.AreEqual(TriangularNumber(numberOfItems), Flatten(receivedItems).Sum(i => i));
+        }
+
+        [Test]
+        public async Task Pushing_for_slots_after_start_works_when_batch_size_is_reached()
+        {
+            var receivedItems = new ConcurrentQueue<List<int>>[4]
+            {
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+            };
+
+            var countDownEvent = new CountdownEvent(16);
+
+            // choose insanely high push interval
+            var completion = new MultiProducerConcurrentCompletion<int>(batchSize: 100, pushInterval: TimeSpan.FromDays(1), maxConcurrency: 4, numberOfSlots: 4);
+
+            completion.Start((items, slot, state, token) =>
+            {
+                receivedItems[slot].Enqueue(new List<int>(items)); // take a copy
+                if (!countDownEvent.IsSet)
+                {
+                    countDownEvent.Signal();
+                }
+                return Task.FromResult(0);
+            });
+
+            var numberOfItems = await PushConcurrentlyTwoThousandItemsInPackagesOfFiveHundredIntoFourSlots(completion);
+
+            // we wait for 16 counts and then complete midway
+            await Task.Run(() => countDownEvent.Wait(TimeSpan.FromSeconds(5)));
+
+            await completion.Complete();
+
+            Assert.AreEqual(TriangularNumber(numberOfItems), Flatten(receivedItems).Sum(i => i));
+        }
+
+        [Test]
+        public async Task Pushing_for_slots_before_start_works_when_pushInterval_reached()
+        {
+            var receivedItems = new ConcurrentQueue<List<int>>[4]
+            {
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+            };
+
+            var countDownEvent = new CountdownEvent(4);
+
+            // choose insanely high batchSize
+            var completion = new MultiProducerConcurrentCompletion<int>(batchSize: 10000, pushInterval: TimeSpan.FromMilliseconds(1), maxConcurrency: 4, numberOfSlots: 4);
+
+            var numberOfItems = await PushConcurrentlyTwoThousandItemsInPackagesOfFiveHundredIntoFourSlots(completion);
+
+            completion.Start((items, slot, state, token) =>
+            {
+                receivedItems[slot].Enqueue(new List<int>(items)); // take a copy
+                if (!countDownEvent.IsSet)
+                {
+                    countDownEvent.Signal();
+                }
+                return Task.FromResult(0);
+            });
+
+            await Task.Run(() => countDownEvent.Wait(TimeSpan.FromSeconds(5)));
+
+            var sumOfAllElementsSeenSoFar = Flatten(receivedItems).Sum(i => i);
+
+            await completion.Complete();
+
+            Assert.AreEqual(TriangularNumber(numberOfItems), sumOfAllElementsSeenSoFar);
+            Assert.AreEqual(TriangularNumber(numberOfItems), Flatten(receivedItems).Sum(i => i), "Dispatcher complete brought more items than expected");
+        }
+
+        [Test]
+        public async Task Pushing_for_slots_after_start_works_when_pushInterval_reached()
+        {
+            var receivedItems = new ConcurrentQueue<List<int>>[4]
+            {
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+            };
+
+            var countDownEvent = new CountdownEvent(4);
+
+            // choose insanely high batchSize to force push interval picking up all the content
+            var completion = new MultiProducerConcurrentCompletion<int>(batchSize: 10000, pushInterval: TimeSpan.FromMilliseconds(1), maxConcurrency: 4, numberOfSlots: 4);
+
+            completion.Start((items, slot, state, token) =>
+            {
+                receivedItems[slot].Enqueue(new List<int>(items)); // take a copy
+                if (!countDownEvent.IsSet)
+                {
+                    countDownEvent.Signal();
+                }
+                return Task.FromResult(0);
+            });
+
+            var numberOfItems = await PushConcurrentlyTwoThousandItemsInPackagesOfFiveHundredIntoFourSlots(completion);
+
+            await Task.Run(() => countDownEvent.Wait(TimeSpan.FromSeconds(5)));
+
+            var sumOfAllElementsSeenSoFar = Flatten(receivedItems).Sum(i => i);
+
+            await completion.Complete();
+
+            Assert.AreEqual(TriangularNumber(numberOfItems), sumOfAllElementsSeenSoFar);
+            Assert.AreEqual(TriangularNumber(numberOfItems), Flatten(receivedItems).Sum(i => i), "Dispatcher complete brought more items than expected");
+        }
+
+        [Test]
+        public async Task Pushing_and_complete_with_no_drain_without_start_empties_slots()
+        {
+            var pushedItems = new ConcurrentQueue<List<int>>[4]
+            {
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+            };
+
+            // choose insanely high batchSize to force push interval picking up all the content
+            var completion = new MultiProducerConcurrentCompletion<int>(batchSize: 100, pushInterval: TimeSpan.FromMilliseconds(1), maxConcurrency: 4, numberOfSlots: 4);
+
+            await PushConcurrentlyTwoThousandItemsInPackagesOfFiveHundredIntoFourSlots(completion);
+
+            await completion.Complete(drain: false);
+
+            var countDownEvent = new CountdownEvent(1);
+            completion.Start((items, slot, state, token) =>
+            {
+                pushedItems[slot].Enqueue(new List<int>(items)); // take a copy
+                countDownEvent.Signal();
+                return Task.FromResult(0);
+            });
+
+            completion.Push(1, slotNumber: 1);
+
+            await Task.Run(() => countDownEvent.Wait(TimeSpan.FromSeconds(5)));
+
+            await completion.Complete();
+
+            Assert.AreEqual(1, Flatten(pushedItems).Sum(i => i));
+        }
+
+        [Test]
+        public async Task Complete_works_even_when_push_interval_and_batch_size_not_reached()
+        {
+            var receivedItems = new ConcurrentQueue<List<int>>[4]
+            {
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+                new ConcurrentQueue<List<int>>(),
+            };
+
+            // choose insanely high batchSize and pushInterval to force the loop hanging
+            var completion = new MultiProducerConcurrentCompletion<int>(batchSize: 10000, pushInterval: TimeSpan.FromDays(1), maxConcurrency: 4, numberOfSlots: 4);
+
+            completion.Start((items, slot, state, token) =>
+            {
+                receivedItems[slot].Enqueue(new List<int>(items)); // take a copy
+                return Task.FromResult(0);
+            });
+
+            var numberOfItems = await PushConcurrentlyTwoThousandItemsInPackagesOfFiveHundredIntoFourSlots(completion);
+
+            await completion.Complete();
+
+            var sumOfAllElementsSeenSoFar = Flatten(receivedItems).Sum(i => i);
+
+            Assert.AreEqual(TriangularNumber(numberOfItems), sumOfAllElementsSeenSoFar);
+            Assert.AreEqual(TriangularNumber(numberOfItems), Flatten(receivedItems).Sum(i => i), "Dispatcher complete brought more items than expected");
+        }
+
+        static async Task<int> PushConcurrentlyTwoThousandItemsInPackagesOfFiveHundredIntoFourSlots(MultiProducerConcurrentCompletion<int> completion)
+        {
+            var t1 = Task.Run(() => Parallel.For(1, 500, i =>
+            {
+                completion.Push(slotNumber: 0, item: i);
+            }));
+
+            var t2 = Task.Run(() => Parallel.For(500, 1000, i =>
+            {
+                completion.Push(slotNumber: 1, item: i);
+            }));
+
+            var t3 = Task.Run(() => Parallel.For(1000, 1500, i =>
+            {
+                completion.Push(slotNumber: 2, item: i);
+            }));
+
+            await Task.WhenAll(t1, t2, t3);
+
+            var numberOfItems = 2000;
+            for (var i = 1500; i < numberOfItems + 1; i++)
+            {
+                completion.Push(slotNumber: 3, item: i);
+            }
+            return numberOfItems;
+        }
+
+        int TriangularNumber(int numberOfItems)
+        {
+            return numberOfItems * (numberOfItems + 1) / 2;
+        }
+
+        IEnumerable<int> Flatten(ConcurrentQueue<List<int>>[] captured)
+        {
+            var allCaptured = new List<int>();
+            foreach (var queue in captured)
+            {
+                foreach (var list in queue.ToArray())
+                {
+                    allCaptured.AddRange(list);
+                }
+            }
+            return allCaptured.OrderBy(i => i);
+        }
+    }
+}
